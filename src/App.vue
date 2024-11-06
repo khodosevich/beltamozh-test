04ce3c7b9db05d0d21ba0c45a46f9e232ad813b4
Ходосевич Матвей <hodos.952795@gmail.com>
2024-11-06 16:36:55 +0300

    init: beltamozh-test project

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup>
+</script>
+
+<template>
+  <header>
+    <div class="wrapper">
+
+    </div>
+  </header>
+
+  <main>
+    init
+  </main>
+</template>
+
+<style>
+@import "styles/reset.scss";
+@import "styles/root.scss";
+
+
+</style>
